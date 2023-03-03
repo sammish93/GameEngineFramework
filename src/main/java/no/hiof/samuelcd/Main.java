f44297f29dd919d52918660063129e91f2313803
Sam Davies <samuelcd@hiof.no>
2023-03-03 09:20:02 +0100

    test commit to repo

--- src/main/java/no/hiof/samuelcd/Main.java
+++ src/main/java/no/hiof/samuelcd/Main.java
@@ -2,10 +2,12 @@ package no.hiof.samuelcd;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Running the game in a terminal.
         var game = new GameEngine("TERMINAL");
         game.run();
 
-
+        // Running the game in Swing.
         var game2 = new GameEngine("SWING");
         game2.run();
 
