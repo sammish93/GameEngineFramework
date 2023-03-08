8a60c0d7f9e9cb8b04212b3c086bc56c756510f4
Sam Davies <samuelcd@hiof.no>
2023-03-08 11:56:18 +0100

    added initial tool classes that may be required at game runtime

---
+++ src/main/java/no/hiof/samuelcd/tbage/tools/CombatTurn.java
@@ -0,0 +1,5 @@
+package no.hiof.samuelcd.tbage.tools;
+
+public class CombatTurn {
+    // This class is used to determine behaviour for a single combat turn.
+}
---
+++ src/main/java/no/hiof/samuelcd/tbage/tools/WeightedProbabilityCalculator.java
@@ -0,0 +1,5 @@
+package no.hiof.samuelcd.tbage.tools;
+
+public class WeightedProbabilityCalculator {
+    // This class is used to determine which items drop, or which encounters are chosen for a single permutation.
+}
