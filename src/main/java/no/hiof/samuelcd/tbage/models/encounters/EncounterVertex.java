dc788c9537afa85f38d7ad35ebcf7c46c6339d3f
Sam Davies <samuelcd@hiof.no>
2023-03-09 14:23:23 +0100

    changed implemented interface from JsonExternalisable to Serializable because of onUse() and onImplementation() custom override philosophy

--- src/main/java/no/hiof/samuelcd/tbage/models/encounters/EncounterVertex.java
+++ src/main/java/no/hiof/samuelcd/tbage/models/encounters/EncounterVertex.java
@@ -1,7 +1,5 @@
 package no.hiof.samuelcd.tbage.models.encounters;
 
-import no.hiof.samuelcd.tbage.models.encounters.Encounter;
-
 public class EncounterVertex {
     public Encounter encounter;
     //public Map<String, String>;
