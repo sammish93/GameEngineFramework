6ba24a9fccac97b83a365253ce2d5ee2751307ad
sammish93 <samd93@hotmail.co.uk>
2023-04-27 13:50:08 +0200

    added additional test

--- src/test/java/sammish93.tbage/tools/StringParserTest.java
+++ src/test/java/sammish93.tbage/tools/StringParserTest.java
@@ -28,4 +28,20 @@ class StringParserTest {
             assertEquals(input, map.get("command"));
         }
     }
+
+    @Test
+    void assertsValidVerbNounInput() {
+        String input = "climb ladder";
+        String[] splitString = input.split("\\s");
+
+        try (MockedStatic<StringParser> stringParser = mockStatic(StringParser.class)) {
+            stringParser.when(() -> StringParser.read(gameEngine, input))
+                    .thenReturn(new TreeMap<String, String>()
+                    {{ put("verb", splitString[0]); put("noun", splitString[1]); }});
+
+            TreeMap<String, String> map = StringParser.read(gameEngine, input);
+            assertEquals("climb", map.get("verb"));
+            assertEquals("ladder", map.get("noun"));
+        }
+    }
 }
